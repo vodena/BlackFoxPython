6117dd13d920bb0f295c4af0d4632bce57be32ca
DjordjeMitrovic <mitrovicdjordje076@gmail.com>
2019-03-18 00:34:57 +0100

    Dodate preglednije poruke o grešci

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = "1.0.0"
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 
-REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil", "PyContracts >= 1.8.12"]
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
 
 setup(
     name=NAME,
